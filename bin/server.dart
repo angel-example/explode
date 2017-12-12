6f3e0eec764275ef9411629e3408750f5cf53c84
Tobe O <thosakwe@gmail.com>
2017-12-12 12:49:42 -0500

    Added hot-reloading

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,19 @@
 #!/usr/bin/env dart
-import 'dart:async';
-import 'common.dart';
+import 'dart:io';
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_hot/angel_hot.dart';
+import 'package:explode/explode.dart' as explode;
 
 main(args) async {
-  runZoned(startServer(args), onError: onError);
+  var hot = new HotReloader(() async {
+    var app = new Angel();
+    await app.configure(explode.configureServer);
+    return app;
+  }, [
+    new Directory('config'),
+    new Directory('lib'),
+  ]);
+
+  var server = await hot.startServer('127.0.0.1', 3000);
+  print('Listening at http://${server.address.address}:${server.port}');
 }
--- lib/explode.dart
+++ lib/explode.dart
@@ -1,19 +1,15 @@
 library explode;
 
 import 'dart:async';
-import 'package:angel_common/angel_common.dart';
+import 'package:angel_framework/angel_framework.dart';
 import 'src/config/config.dart' as configuration;
 import 'src/routes/routes.dart' as routes;
 import 'src/services/services.dart' as services;
 export 'src/services/services.dart';
 
-/// Creates and configures the server instance.
-Future<Angel> createServer() async {
-  Angel app = new Angel();
-
+/// Configures a server instance.
+Future configureServer(Angel app) async {
   await app.configure(configuration.configureServer);
   await app.configure(services.configureServer);
   await app.configure(routes.configureServer);
-
-  return app;
 }
