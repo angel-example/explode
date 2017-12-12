2b4f57d3555c06ca5901211d24e71d355028889a
Tobe O <thosakwe@gmail.com>
2017-12-12 13:03:53 -0500

    Upgraded route configuration

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,56 +1,52 @@
 library explode.routes;
 
-import 'package:angel_common/angel_common.dart';
-import 'controllers/controllers.dart' as Controllers;
-
-configureBefore(Angel app) async {
-  app.before.add(cors());
-}
-
-/// Put your app routes here!
-configureRoutes(Angel app) async {}
-
-configureAfter(Angel app) async {
-  // Static server, and pub serve while in development
-  await app.configure(new PubServeLayer());
-  await app.configure(new VirtualDirectory());
-
-  // Set our application up to handle different errors.
-  var errors = new ErrorHandler(handlers: {
-    404: (req, res) async =>
-        res.render('error', {'message': 'No file exists at ${req.path}.'}),
-    500: (req, res) async => res.render('error', {'message': req.error.message})
-  });
-
-  errors.fatalErrorHandler = (AngelFatalError e) async {
-    var req = await RequestContext.from(e.request, app);
-    var res = new ResponseContext(e.request.response, app);
-    res.render('error', {'message': 'Internal Server Error: ${e.error}'});
-    await app.sendRequest(e.request, req, res);
-  };
-
-  // Throw a 404 if no route matched the request
-  app.after.add(errors.throwError());
-
-  // Handle errors when they occur, based on outgoing status code.
-  // By default, requests will go through the 500 handler, unless
-  // they have an outgoing 200, or their status code has a handler
-  // registered.
-  app.after.add(errors.middleware());
-
-  // Pass AngelHttpExceptions through handler as well
-  await app.configure(errors);
-
-  // Compress via GZIP
-  // Ideally you'll run this on a `multiserver` instance, but if not,
-  // feel free to knock yourself out!
-  //
-  app.responseFinalizers.add(gzip());
-}
+import 'dart:io';
+import 'package:angel_cors/angel_cors.dart';
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_proxy/angel_proxy.dart';
+import 'package:angel_static/angel_static.dart';
+import 'package:file/local.dart';
+import 'package:http/http.dart' as http;
 
 configureServer(Angel app) async {
-  await configureBefore(app);
-  await configureRoutes(app);
-  await app.configure(Controllers.configureServer);
-  await configureAfter(app);
+  // Enable CORS
+  app.use(cors());
+
+  // Enable GZIP/DEFLATE compression
+  app.injectEncoders({
+    'gzip': GZIP.encoder,
+    'deflate': ZLIB.encoder,
+  });
+
+  // In development, mount a reverse proxy over pub serve
+  if (!app.isProduction) {
+    var proxy = new Proxy(app, new http.Client(), 'http://localhost:8080');
+
+    // Dispose of it when the server is shut down
+    app.shutdownHooks.add((_) async => proxy.close());
+
+    // Send fallback requests to the reverse proxy
+    app.use(proxy.handleRequest);
+  }
+
+  // Mount a static server over web/.
+  //
+  // In production, this automatically mounts over build/web/.
+  var vDir = new VirtualDirectory(app, const LocalFileSystem());
+  app.use(vDir.handleRequest);
+
+  // Any request that isn't handled by a route, the reverse proxy, or the
+  // static server should be handled as a 404.
+  app.use((RequestContext req, ResponseContext res) {
+    return res.render('error', {'message': 'No file exists at ${req.path}.'});
+  });
+
+  // Override the default application error handler with one that renders
+  // an error page.
+  var oldHandler = app.errorHandler;
+  app.errorHandler = (e, req, res)  {
+    if (req.accepts('text/html', strict: true))
+      return res.render('error', {'message': e.message});
+    return oldHandler(e, req, res);
+  };
 }
